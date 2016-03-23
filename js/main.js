a16031d084adb6a93646ba5f19a89101e5c735a8
dhkstn1991 <dhkstn1991@naver.com>
2016-03-23 07:00:08 +0000

    + - 기능 구현
    git-svn-id: https://dev.naver.com/svn/scrc0322/trunk@3 f89e85a8-982e-0510-ba75-f677e5c4b7dd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+$(function(){
+	// 드래그 기능 활용 시 필요한 부분
+
+	jQuery.event.props.push('dataTransfer');
+
+	var body = jQuery('body')
+		.bind( 'dragenter dragover', false);
+});
+
 $(function() {
+	// 탭(스크립트, 모양, 소리) 구현부
+
 	$("#work-space nav a:first").addClass("active");
 	$("#work-space #script-tab").show();
 	
@@ -8,5 +19,87 @@ $(function() {
 		
 		$(this).addClass("active");
 		$("#work-space " + this.target).fadeIn();
+	});
+});
+
+$(function () {
+	// code-piece의 드래그 앤 드랍 기능 구현부
+
+	$(".code-piece").attr("draggable", true);
+	$(".code-piece").on("dragstart", drag);
+
+	$("#blocks").on("drop", drop);
+	$("#code-sec").on("drop", drop);
+
+	//드래그 시작시 호출 할 함수
+	function drag(event) {
+		event.dataTransfer.setData('piece', this.id);
+	}
+
+	//드롭시 호출 할 함수
+	// TODO: 드롭시 엘리먼트롤 복사해 붙혀넣는 기능으로 바꿔야 한다.
+	function drop(event) {
+		var id = event.dataTransfer.getData('piece');
+
+		this.appendChild(document.getElementById(id));
+		event.preventDefault();
+	}
+});
+
+$(function () {
+	// 여러가지 기본값
+	$(".default.element.number").val(0);
+});
+
+$(function () {
+	// 코드 조각들을 클릭하면 코드가 동작한다.
+	// TODO: 말풍선으로 출력하도록 바꾼다.
+	// TODO: 제어부의 경우 타이머를 통해 불연속적으로 동작해야 하므로 바꿔야 한다.
+	$(".code-piece.operator").click(function (event) {
+		if ($(event.target).hasClass("operator")) {
+			console.log(this.calc());
+		}
+	});
+});
+
+$(function () {
+	// 여러 엘리먼트들이 들어갈 수 있는 빈공간.
+	// 내부에는 '하나'의 엘리먼트만 들어갈 수 있다.
+	$(".element.space").each(function () {
+		this.calc = function () {
+			var $elmt = $(this).find(".element");
+
+			return $elmt[0].calc();
+		};
+	});
+
+	// number는 default element 중에 하나인데
+	// 따로 존재할 수 업고 항상 다른 엘리먼트의 안에 있다.
+	// TODO: space element가 텅 비어 있으면 나타나고, 아니면 숨겨지는 식으로 구현할 것.
+	$(".element.number").each(function () {
+		this.calc = function () {
+			return parseInt(this.value);
+		};
+	});
+});
+
+$(function () {
+	// plus element의 기능 구현(기타 기능 구현도 여기에 하면 좋을 것이다)
+	// 내부에 2개의 space element가 있고 그것을 더하는 element이다.
+	$(".element.plus").each(function () {
+		this.calc = function () {
+			var $space = $(this).find(".space");
+
+			return $space[0].calc() + $space[1].calc();
+		};
+	});
+
+	// minus element의 기능 구현
+	$(".element.minus").each(function () {
+		this.calc = function () {
+			var $space = $(this).find(".space");
+
+			return $space[0].calc() - $space[1].calc();
+		};
 	});
 });
