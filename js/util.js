21553964337957a61a9c5ca132486ff8852cbc07
dhkstn1991 <dhkstn1991@naver.com>
2016-05-06 11:50:42 +0000

    캔버스 - 점 생성 이동 각도변경 그 외 namespace 추가 등
    git-svn-id: https://dev.naver.com/svn/scrc0322/trunk@9 f89e85a8-982e-0510-ba75-f677e5c4b7dd

--- js/util.js
+++ js/util.js
@@ -3,10 +3,31 @@
  */
 var scrc;
 scrc = scrc || {};
-scrc.util = scrc.util || {};
+
+scrc.namespace = function (ns_string) {
+    var parts = ns_string.split('.'),
+        parent = scrc,
+        i;
+
+    if (parts[0] === "scrc") {
+        parts = parts.slice(1);
+    }
+
+    for (i = 0; i < parts.length; i++) {
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+
+        parent = parent[parts[i]];
+    }
+
+    return parent;
+};
 
 $(function () {
-    scrc.util.uniqueId = (function(){
+    var util = scrc.namespace("util");
+
+    util.uniqueId = (function(){
         var id=0;
 
         return function(){ return "--uq--id--" + id++;}
