78af51d8906eaaaaad6a67516b52b49ad5d89fcf
dhkstn1991 <dhkstn1991@naver.com>
2016-05-16 10:35:00 +0000

    캔버스 기능 합병
    git-svn-id: https://dev.naver.com/svn/scrc0322/branches/hws_branch@50 f89e85a8-982e-0510-ba75-f677e5c4b7dd

--- js/main-screen.js
+++ js/main-screen.js
@@ -23,6 +23,7 @@ $(function () {
         x : 100,
         y : 100,
         angle : 0,
+        img : null,
         move : function (x, y) {
             this.x += x;
             this.y += y;
@@ -40,15 +41,97 @@ $(function () {
         ctx.translate(p.x, p.y);
         ctx.rotate(p.angle);
         ctx.translate(-p.x, -p.y);
-        ctx.beginPath();
-        ctx.moveTo(p.x, p.y);
-        ctx.lineTo(p.x - 20, p.y - 10);
-        ctx.lineTo(p.x - 20, p.y + 10);
-        ctx.closePath();
-        ctx.fillStyle = "red";
-        ctx.fill();
+        if (p.img == null) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.x - 20, p.y - 10);
+            ctx.lineTo(p.x - 20, p.y + 10);
+            ctx.closePath();
+            ctx.fillStyle = "red";
+            ctx.fill();
+        } else {
+            var img = p.img;
+
+            ctx.drawImage(img, p.x - p.width / 2, p.y - p.height / 2, p.width, p.height);    // 이미지 위치x, 위치y, 크기x, 크기y
+        }
         ctx.restore();
     };
 
     main_screen.draw();
+});
+
+$(function(){
+    var main_screen = scrc.namespace("main_screen");
+
+    $("#stage input").on("change", handleFiles);
+
+    function handleFiles(e) {
+        var img = new Image;
+        img.src = URL.createObjectURL(e.target.files[0]);
+
+        main_screen.one_point.img = img;
+        main_screen.one_point.width = 128;
+        main_screen.one_point.height = 120;
+
+        img.onload = function() {
+            main_screen.draw();
+        };
+
+        var canvasOffset=$("#main-screen").offset();
+        var offsetX=canvasOffset.left;
+        var offsetY=canvasOffset.top;
+
+        var p = main_screen.one_point;
+        var isDragging=false;
+
+        function handleMouseDown(e){
+            if (!isDragging) {
+                p.x = parseInt(e.clientX - offsetX);
+                p.y = parseInt(e.clientY - offsetY);
+                // set the drag flag
+                isDragging = true;
+
+                main_screen.draw();
+            }
+        }
+
+        function handleMouseUp(e){
+            if (isDragging) {
+                p.x = parseInt(e.clientX - offsetX);
+                p.y = parseInt(e.clientY - offsetY);
+                // clear the drag flag
+                isDragging = false;
+
+                main_screen.draw();
+            }
+        }
+
+        function handleMouseOut(e){
+            if(isDragging) {
+                p.x = parseInt(e.clientX - offsetX);
+                p.y = parseInt(e.clientY - offsetY);
+                // user has left the canvas, so clear the drag flag
+                //isDragging=false;
+
+                main_screen.draw();
+            }
+        }
+
+        function handleMouseMove(e){
+            // if the drag flag is set, clear the canvas and draw the image
+            if(isDragging){
+                p.x = parseInt(e.clientX-offsetX);
+                p.y = parseInt(e.clientY-offsetY);
+                /*ctx.clearRect(0,0,canvasWidth,canvasHeight);
+                ctx.drawImage(img,canMouseX-128/2,canMouseY-120/2,128,120);*/
+
+                main_screen.draw();
+            }
+        }
+
+        $("#main-screen").mousedown(handleMouseDown);
+        $("#main-screen").mousemove(handleMouseMove);
+        $("#main-screen").mouseup(handleMouseUp);
+        $("#main-screen").mouseout(handleMouseOut);
+    }
 });
