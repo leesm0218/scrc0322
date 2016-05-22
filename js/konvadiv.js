b86d7698599c78bd7e79aa37b0954d54b0170888
whlwlssja121 <whlwlssja121@naver.com>
2016-05-22 06:30:05 +0000

    git-svn-id: https://dev.naver.com/svn/scrc0322/trunk@58 f89e85a8-982e-0510-ba75-f677e5c4b7dd

---
+++ js/konvadiv.js
@@ -0,0 +1,108 @@
+/* 2016-05-22 by Choi */
+
+// http://konvajs.github.io/docs/index.html
+
+// create stage canvas
+var width = 400;
+var height = 300;
+
+var stage = new Konva.Stage({
+    container: 'container',
+    width: width,
+    height: height
+});
+
+// create image layer
+var layer = new Konva.Layer();
+
+$("#stage input").on("change", handleFiles);
+
+function handleFiles(e) {
+
+    var img = new Image();
+    img.src = URL.createObjectURL(e.target.files[0]);
+
+    img.onload = function () {
+        var myimg = new Konva.Image({
+            x: 50,
+            y: 50,
+            image: img,
+            draggable: true,
+            width: 100,
+            height: 100,
+            stroke:"red",
+            strokeWidth: 2,
+            strokeEnabled: false
+
+        })
+        layer.add(myimg);
+        layer.draw();
+        myimg.on("dragstart", function () {
+            this.moveToTop();
+            layer.draw();
+        });
+    }
+}
+
+
+
+//  rectangles for test
+
+var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+for(var i = 0; i < 6; i++) {
+    var box = new Konva.Rect({
+        x: i * 30 + 50,
+        y: i * 18 + 40,
+        fill: colors[i],
+        draggable: true,
+        width: 100,
+        height: 50,
+        stroke:"red",
+        strokeWidth: 2,
+        strokeEnabled: false
+    });
+
+    box.on("dragstart", function() {
+        this.moveToTop();
+        layer.draw();
+    });
+
+    box.on("dragmove", function() {
+        document.body.style.cursor = "pointer";
+    });
+    /*
+     * dblclick to remove box for desktop app
+     * and dbltap to remove box for mobile app
+     */
+    box.on("dblclick dbltap", function() {
+        this.destroy();
+        layer.draw();
+    });
+
+    box.on("mouseover", function() {
+        document.body.style.cursor = "pointer";
+    });
+    box.on("mouseout", function() {
+        document.body.style.cursor = "default";
+    });
+
+    layer.add(box);
+}
+
+
+//mouseover stroke event
+
+layer.on('mouseover', function(evt) {
+    var shape = evt.target;
+    shape.strokeEnabled(true);
+    layer.draw();
+});
+layer.on('mouseout', function(evt) {
+    var shape = evt.target;
+    shape.strokeEnabled(false);
+    layer.draw();
+});
+
+// add the layer to the stage
+stage.add(layer);
